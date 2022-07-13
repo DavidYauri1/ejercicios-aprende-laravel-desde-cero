fc396bd88e8f082e4b3f3f6bbde8d16056582586
DavidYauri1 <david.yauri@tecsup.edu.pe>
2022-07-13 18:53:05 -0500

    Solucion

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,55 @@ Route::get('/ejercicio1', function () {
 Route::post('/ejercicio1', function () {
     return "POST OK";
 });
+
+
+Route::post('/ejercicio2/a', function (Request $request) {
+    return Response::json([
+        'name' =>'Keyboard',
+        'description' =>'Mechanical RGB keyboard',
+        'price' => $request->get('price'),
+    ]);
+});
+
+Route::post('/ejercicio2/b', function (Request $request) {
+    if( $request->get('price') < 0) {
+            return Response::json(['message' => "Price can't be less than 0"])->setStatusCode(422);
+
+        } ;
+ 
+    return Response::json(
+        [
+            'name' => 'Keyboard',
+            'description' => 'Mechanical RGB keyboard ',
+            'price' => $request->get('price'),
+        ]
+        );
+
+}); 
+
+  
+Route::post('/ejercicio2/c' , function(Request $request){
+        $descount = 0;
+
+        if( $request->get('discount') == "SAVE5"){
+             $descount=5;
+        }
+        else if ( $request->get('discount') == "SAVE10"){
+             $descount=10;
+        }
+        else if ( $request->get('discount') == "SAVE15"){
+             $descount=15;
+        }
+        
+        //calculation price
+        $price = (100- $descount)/ 100 * $request->get('price');
+
+        return Response::json(
+            [
+                'name' => 'Keyboard',
+                'description' => 'Mechanical RGB keyboard',
+                'price' =>$price,
+                'discount' =>$descount
+            ]
+            );
+});
